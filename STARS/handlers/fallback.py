c7a3078956c7ca617762db4e08cc5d267d567822
makman02 <makman2006@mail.ru>
2025-08-07 13:33:13 +0300

    Delete STARS directory

--- STARS/handlers/fallback.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router, types
-
-router = Router()
-
-@router.message()
-async def fallback(message: types.Message):
-    await message.answer("Я не понимаю это сообщение. Используй команды или кнопки меню.")
